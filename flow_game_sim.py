6107f60b5fa95ecb071d9650c0749ed6a61af6cf
Tanay Lathia <lathiata@berkeley.edu>
2016-07-15 23:48:44 -0700

    - Add arg parse to flow_game_sim 	-have two cl args: difficulty and transition model 	-does validation on input args

--- flow_game_sim.py
+++ flow_game_sim.py
@@ -1,21 +1,22 @@
+import argparse
+import random
 from flow_game_state import FlowGameState
 from flow_game_problem import FlowGameProblem
 from flow_search import *
 from flow_game_constants import TransitionModels
 
-# TODO: make a command line argument parser to specify search model and problem difficulty
-# TODO: specify problems by difficuly 
-# TODO: change VERSION to specify the search type (min conflicts, greedy, k-beams, etc)
-
-PROBLEM_NUM = 0 #indexes into problems list below
+# TODO: change VERSION to specify the search type (min conflicts, flow_search, k-beams, etc)
+#		will be interesting bc VERSION refers to transition model but these are search algs
 PRINT = False
-VERSION = TransitionModels.VERSION_1
 
-problems = [FlowGameProblem(5, [[(0,2),(3,0)], [(0,3),(4,3)], [(1,3),(2,1)], [(3,3),(4,4)], [(3,1),(4,0)]]),
-			FlowGameProblem(8, [[(0,0),(3,0)], [(4,0),(7,6)], [(2,1),(3,5)], [(2,2),(1,6)], [(2,3),(3,6)], [(4,1),(6,1)],
-						[(3,1), (4,4)], [(4,3),(6,6)], [(5,3),(7,7)]]),
-			FlowGameProblem(8, [[(1,6),(2,1)], [(2,2),(7,3)], [(3,1),(6,6)], [(4,2),(7,7)], [(5,4),(6,5)], [(6,1),(7,6)]]),
-			FlowGameProblem(8, [[(0,0),(5,2)], [(1,1),(3,0)], [(1,2),(2,6)], [(1,6),(5,4)], [(4,0),(5,3)]])]
+EASY_PUZZLES = [FlowGameProblem(5, [[(0,2),(3,0)], [(0,3),(4,3)], [(1,3),(2,1)], [(3,3),(4,4)], [(3,1),(4,0)]])]
+MEDIUM_PUZZLES = [FlowGameProblem(8, [[(0,0),(3,0)], [(4,0),(7,6)], [(2,1),(3,5)], [(2,2),(1,6)], [(2,3),(3,6)], [(4,1),(6,1)],
+							 		 [(3,1), (4,4)], [(4,3),(6,6)], [(5,3),(7,7)]])]
+HARD_PUZZLES = [FlowGameProblem(8, [[(1,6),(2,1)], [(2,2),(7,3)], [(3,1),(6,6)], [(4,2),(7,7)], [(5,4),(6,5)], [(6,1),(7,6)]]),
+				FlowGameProblem(8, [[(0,0),(5,2)], [(1,1),(3,0)], [(1,2),(2,6)], [(1,6),(5,4)], [(4,0),(5,3)]])]
+
+DIFFICULTY = {"easy": EASY_PUZZLES, "medium": MEDIUM_PUZZLES, "hard": HARD_PUZZLES}
+SEARCH_MODEL = {"single": TransitionModels.VERSION_1, "multi": TransitionModels.VERSION_0}
 
 def test(version, problem):
 	print(problem.get_start_state())
@@ -23,4 +24,23 @@ def test(version, problem):
 	result = flow_game_search(problem, version, PRINT)
 	print(result)
 
-test(VERSION, problems[PROBLEM_NUM])
+def validate_args(args):
+	'''
+	returns bool, problem, search_model
+	'''
+	if args.difficulty not in DIFFICULTY.keys() or args.search_model not in SEARCH_MODEL.keys():
+		return False, None, None
+
+	return True, random.choice(DIFFICULTY[args.difficulty]), SEARCH_MODEL[args.search_model]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("difficulty", help="complexity of puzzle: easy, medium, hard")
+parser.add_argument("search_model", help="name of search model: single, multi")
+args = parser.parse_args()
+success, problem, version = validate_args(args)
+
+if success:
+	test(version, problem)
+else:
+	print("use the -h flag to see help for valid inputs")
